dea6460df187cc3684eb054f47f724df651188ab
mariasharmagbay <mariasharmagbay@gmail.com>
2021-07-26 13:15:08 -0400

    Initial commit

---
+++ login_register_modal.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Login/Register</title>
+
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+
+	<style>body{padding-top: 60px;}</style>
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+
+	<link href="assets/css/login-register.css" rel="stylesheet" />
+	<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+
+	<script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+	<script src="assets/js/bootstrap.js" type="text/javascript"></script>
+	<script src="assets/js/login-register.js" type="text/javascript"></script>
+
+</head>
+<body>
+    <div class="image-container set-full-height" style="background-image: url('images/mentalillness_hand4.jpg')">
+    <!--<div class="container"> -->
+
+		 <div class="modal fade login" id="loginModal">
+		      <div class="modal-dialog login animated">
+    		      <div class="modal-content">
+    		         <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title">Login</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="box">
+                             <div class="content">
+                                <div class="error"></div>
+                                <div class="form loginBox">
+                                    <form method="" action="" accept-charset="UTF-8">
+                                    <input id="email" class="form-control" type="text" placeholder="Email" name="email">
+                                    <input id="password" class="form-control" type="password" placeholder="Password" name="password">
+                                    <input class="btn btn-default btn-login" type="button" value="Login" onclick="loginAjax()">
+                                    </form>
+                                </div>
+                             </div>
+                        </div>
+                        <div class="box">
+                            <div class="content registerBox" style="display:none;">
+                             <div class="form">
+                                <form method="" html="{:multipart=>true}" data-remote="true" action="" accept-charset="UTF-8">
+                                    <input id="firstname" class="form-control" type="text" placeholder="First Name" name="firstname">
+                                    <input id="lastname" class="form-control" type="text" placeholder="Last Name" name="lastname">
+                                    <input id="email" class="form-control" type="text" placeholder="Email" name="email">
+                                    <input id="password" class="form-control" type="password" placeholder="Password" name="password">
+                                    <input id="password_confirmation" class="form-control" type="password" placeholder="Confirm Password" name="password_confirmation">
+                                    <div>
+                                    <input id="agree" class="form-control" type="checkbox"  name="agree">
+                                        <label for="agree"> Do you agree with the terms and conditions? </label>
+                                    </input>
+                                    </div>
+                                    </br>
+                                    <input class="btn btn-default btn-register" type="button" value="Create account" name="commit">
+                                </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="modal-footer">
+                        <div class="forgot login-footer">
+                            <span>Looking to
+                                 <a href="javascript: showRegisterForm();">create an account</a>
+                            ?</span>
+                        </div>
+                        <div class="forgot register-footer" style="display:none">
+                             <span>Already have an account?</span>
+                             <a href="javascript: showLoginForm();">Login</a>
+                        </div>
+                    </div>
+    		      </div>
+		      </div>
+		  </div>
+    </div>
+<script type="text/javascript">
+    $(document).ready(function(){
+        openLoginModal();
+    });
+</script>
+
+
+</body>
+</html>
